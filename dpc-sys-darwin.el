ec51a833c5e6482c7c8a7790d84bf92723e670b4
DinoChiesa <dchiesa@google.com>
2026-05-09 11:16:03 -0700

    docs: adding some info to dpc-sys-darwin.el

--- dpc-sys-darwin.el
+++ dpc-sys-darwin.el
@@ -32,4 +32,22 @@
   (path-helper-setenv "PATH"))
 
 
+
+;; Not sure any of this is necessary. It seems to just work without any
+;; of this customization. Maybe this is legacy from very old emacs.
+
+;; (setq select-enable-clipboard t)
+;; (defun copy-from-osx ()
+;;   (shell-command-to-string "pbpaste"))
+;;
+;; (defun paste-to-osx (text &optional push)
+;;   (let ((process-connection-type nil))
+;;     (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+;;       (process-send-string proc text)
+;;       (process-send-eof proc))))
+;;
+;; (setq interprogram-cut-function 'paste-to-osx)
+;; (setq interprogram-paste-function 'copy-from-osx)
+
+
 (provide 'dpc-sys-darwin)
